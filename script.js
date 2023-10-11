df2da9cc460dc5e466ed3a0886b55aa324874c9c
darrionn33 <darrionn33@gmail.com>
2023-10-11 09:38:26 +0530

    added a color picker

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const boardContainer = document.querySelector(".board-container");
 
 for (let i = 1; i <= 16; i++) {
   const div = document.createElement("div");
+  const colorInput = document.querySelector("input");
   div.style.display = "flex";
 
   for (let i = 1; i <= 16; i++) {
@@ -9,7 +10,7 @@ for (let i = 1; i <= 16; i++) {
     div2.classList.add("grid-on", "grid-off");
     div.append(div2);
     div2.addEventListener("click", () => {
-      div2.classList.toggle("grid-on");
+      div2.style.backgroundColor = colorInput.value;
     });
   }
   boardContainer.append(div);
