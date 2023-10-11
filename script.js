c03b11ec79b559d39f18cd0ad5437cb6874d361e
darrionn33 <darrionn33@gmail.com>
2023-10-11 19:21:35 +0530

    added geid size selector

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+const gridInput = document.querySelector("#gridInput");
+const showGrid = document.querySelector("button");
+const h2 = document.querySelector("h2");
+let gridSize = 16;
+h2.textContent = gridInput.value + ` x ${gridInput.value}`;
+
 const boardContainer = document.querySelector(".board-container");
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= gridSize; i++) {
   const div = document.createElement("div");
-  const colorInput = document.querySelector("input");
+  const colorInput = document.querySelector("#colorInput");
   div.style.display = "flex";
 
-  for (let i = 1; i <= 16; i++) {
+  for (let i = 1; i <= gridSize; i++) {
     const div2 = document.createElement("div");
-    div2.classList.add("grid-on", "grid-off");
+    div2.classList.add("grid");
     div.append(div2);
     div2.addEventListener("click", () => {
       div2.style.backgroundColor = colorInput.value;
@@ -15,3 +21,33 @@ for (let i = 1; i <= 16; i++) {
   }
   boardContainer.append(div);
 }
+gridInput.addEventListener("input", (e) => {
+  h2.textContent = gridInput.value + ` x ${gridInput.value}`;
+  gridSize = gridInput.value;
+  boardContainer.replaceChildren();
+  for (let i = 1; i <= gridSize; i++) {
+    const div = document.createElement("div");
+    const colorInput = document.querySelector("#colorInput");
+    div.style.display = "flex";
+
+    for (let i = 1; i <= gridSize; i++) {
+      const div2 = document.createElement("div");
+      div2.classList.add("grid");
+      div2.style.outline = "1px solid black";
+      div2.style.width = 300 / gridSize + "px";
+      div2.style.height = 300 / gridSize + "px";
+      div.append(div2);
+      div2.addEventListener("click", () => {
+        div2.style.backgroundColor = colorInput.value;
+      });
+    }
+    boardContainer.append(div);
+  }
+});
+showGrid.addEventListener("click", () => {
+  const gridItems = document.querySelectorAll(".grid");
+
+  for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].classList.toggle("lines");
+  }
+});
