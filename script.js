dfe5bc9f7e1a29c9e5c01042b32440944f704ffe
darrionn33 <darrionn33@gmail.com>
2023-10-11 23:20:52 +0530

    cleaned up the code

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-const gridInput = document.querySelector("#gridInput");
-const resetGrid = document.querySelector("button");
-const h2 = document.querySelector("h2");
-let gridSize = 16;
-h2.textContent = gridInput.value + ` x ${gridInput.value}`;
-
+const gridInput = document.querySelector(".grid-input");
+const resetGridButton = document.querySelector(".reset-button");
 const boardContainer = document.querySelector(".board-container");
+const gridSizeDisplay = document.querySelector(".grid-size-display");
+const colorInput = document.querySelector(".color-input");
 
-for (let i = 1; i <= gridSize; i++) {
-  const div = document.createElement("div");
-  const colorInput = document.querySelector("#colorInput");
-  div.style.display = "flex";
+gridSizeDisplay.textContent = gridInput.value + ` x ${gridInput.value}`;
 
-  for (let i = 1; i <= gridSize; i++) {
-    const div2 = document.createElement("div");
-    div2.classList.add("grid");
-    div.append(div2);
-    div2.addEventListener("click", () => {
-      div2.style.backgroundColor = colorInput.value;
-    });
-  }
-  boardContainer.append(div);
-}
-gridInput.addEventListener("input", (e) => {
-  h2.textContent = gridInput.value + ` x ${gridInput.value}`;
-  gridSize = gridInput.value;
-  boardContainer.replaceChildren();
-  for (let i = 1; i <= gridSize; i++) {
+const createGrid = (gridSize) => {
+  for (let i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
-    const colorInput = document.querySelector("#colorInput");
     div.style.display = "flex";
 
-    for (let i = 1; i <= gridSize; i++) {
+    for (let i = 0; i < gridSize; i++) {
       const div2 = document.createElement("div");
-      div2.classList.add("grid");
-      // div2.style.outline = "1px solid lightgray";
+      div2.classList.add("grid-items");
       div2.style.width = 300 / gridSize + "px";
       div2.style.height = 300 / gridSize + "px";
       div.append(div2);
@@ -43,10 +23,19 @@ gridInput.addEventListener("input", (e) => {
     }
     boardContainer.append(div);
   }
+};
+
+createGrid(gridInput.value);
+
+gridInput.addEventListener("input", () => {
+  gridSizeDisplay.textContent = gridInput.value + ` x ${gridInput.value}`;
+  gridSize = gridInput.value;
+  boardContainer.replaceChildren();
+  createGrid(gridInput.value);
 });
 
-resetGrid.addEventListener("click", () => {
-  const gridItems = document.querySelectorAll(".grid");
+resetGridButton.addEventListener("click", () => {
+  const gridItems = document.querySelectorAll(".grid-items");
   for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].style.backgroundColor = "#98bad5";
   }
