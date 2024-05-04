7561ce2e9eac5c77f5f5da7c94758cc69d880e23
darrionn33 <darrionn33@gmail.com>
2024-05-04 23:28:45 +0530

    Converted the code to jQuery

--- script.js
+++ script.js
@@ -1,68 +1,81 @@
-const boardContainer = document.querySelector(".board-container");
-const gridInput = document.querySelector(".grid-input");
-const gridSizeDisplay = document.querySelector(".grid-size-display");
-const penColorInput = document.querySelector(".pen-color-input");
-const gridColorInput = document.querySelector(".grid-color-input");
-const resetGridButton = document.querySelector(".reset-grid-button");
-const gridLineButton = document.querySelector(".grid-line-button");
+const [
+  boardContainer,
+  gridInput,
+  gridSizeDisplay,
+  penColorInput,
+  gridColorInput,
+  resetGridButton,
+  gridLineButton,
+] = [
+  $(".board-container"),
+  $(".grid-input"),
+  $(".grid-size-display"),
+  $(".pen-color-input"),
+  $(".grid-color-input"),
+  $(".reset-grid-button"),
+  $(".grid-line-button"),
+];
 
-let gridLines = false;
-
-gridSizeDisplay.textContent = gridInput.value + ` x ${gridInput.value}`;
+const setGridSizeDisplay = () => {
+  gridSizeDisplay.text(`${gridInput.val()} X ${gridInput.val()}`);
+};
+setGridSizeDisplay();
 
 const createGrid = (gridSize) => {
+  boardContainer.empty();
+  const itemSize = boardContainer.width() / gridSize + "px";
+  const itemColor = gridColorInput.val();
   for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement("div");
-    div.style.display = "flex";
+    boardContainer.append("<div></div>").children().css("display", "flex");
 
-    for (let i = 0; i < gridSize; i++) {
-      const div2 = document.createElement("div");
-      div2.classList.add("grid-items");
-      div2.style.width = boardContainer.offsetWidth / gridSize + "px";
-      div2.style.height = boardContainer.offsetWidth / gridSize + "px";
-      div2.style.backgroundColor = gridColorInput.value;
-      div.append(div2);
-      div2.addEventListener("click", () => {
-        div2.style.backgroundColor = penColorInput.value;
-      });
+    for (let j = 0; j < gridSize; j++) {
+      boardContainer.children().eq(i).append("<div></div>");
+      boardContainer
+        .children()
+        .children()
+        .css({
+          "background-color": itemColor,
+          height: itemSize,
+          width: itemSize,
+        })
+        .addClass("grid-items")
+        .off()
+        .on("click", (e) => {
+          e.target.style.backgroundColor = penColorInput.val();
+        });
     }
-    boardContainer.append(div);
   }
 };
+createGrid(gridInput.val());
 
-createGrid(gridInput.value);
-
-gridInput.addEventListener("input", () => {
-  gridSizeDisplay.textContent = gridInput.value + ` x ${gridInput.value}`;
-  boardContainer.replaceChildren();
-  createGrid(gridInput.value);
+gridInput.on("input", () => {
+  setGridSizeDisplay();
+  createGrid(gridInput.val());
 });
 
 const resetGrid = () => {
-  const gridItems = document.querySelectorAll(".grid-items");
-  for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].style.backgroundColor = gridColorInput.value;
-  }
-};
-resetGridButton.onclick = () => {
-  resetGrid();
+  $(".grid-items").css("background-color", gridColorInput.val());
 };
 
-gridColorInput.onchange = () => {
+resetGridButton.click(() => {
   resetGrid();
-};
+});
+
+gridColorInput.on("change", () => {
+  resetGrid();
+});
+
+let gridLines = false;
 
 const setGridLines = (value) => {
-  const gridItems = document.querySelectorAll(".grid-items");
-  for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].style.outlineStyle = value;
-  }
+  $(".grid-items").css("outline-style", value);
 };
-gridLineButton.onclick = () => {
+
+gridLineButton.click(() => {
   if (gridLines) {
     setGridLines("none");
   } else {
     setGridLines("solid");
   }
   gridLines = !gridLines;
-};
+});
